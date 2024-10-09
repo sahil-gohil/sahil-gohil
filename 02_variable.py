38f6e5b12c45b21ea25aed009c9a6a128041bfcd
sahil-gohil <sahilgohil.code@gmail.com>
2024-10-09 12:26:45 +0000

    Added 02_variable file

---
+++ 02_variable.py
@@ -0,0 +1,26 @@
+# Exercise: Python Variables
+# Create a variable called break and assign it a value 5. See what happens and find out the reason behind the behavior that you see.
+
+# Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables
+
+# Store your first, middle and last name in three different variables and then print your full name using these variables
+
+# Answer which of these are invalid variable names: _nation 1record record1 record_one record-one record^one continue
+
+
+
+
+
+# SOLUTION:
+
+# break = 5
+
+my_year = 2004
+current_year = 2024
+
+my_age = current_year - my_year
+print(my_age)
+
+fname , mname , lname = "sahil " , "ajitbhai ","gohil"
+
+print(fname+mname+lname)
